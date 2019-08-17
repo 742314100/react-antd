3bbb6e9bbceb748ed64d7da0f9fc6764fe631920
liwei <742314100@qq.com>
2019-08-17 08:08:30 +0800

    webstorm github ceshi111

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ export default class App extends Component {
 		return (
 			<div>
                 <div>webstorm1</div>
+				<div>webstorm1</div>
 				<Button type='primary' onClick={this.handle}>primary1</Button>
 			</div>
 		)
