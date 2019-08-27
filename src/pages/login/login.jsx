39a308f5c6edad8692a79a8db3ddfea9867f8c27
742314100@ <742314100@qq.com>
2019-08-27 17:20:19 +0800

    公司git

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,19 +1,67 @@
 import React,{Component} from 'react'
 import './login.less'
-
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
 /*后台管理的路由组件*/
 
-export default class Login extends Component {
+class LoginTag extends Component {
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    };
     render(){
+        const { getFieldDecorator } = this.props.form;
         return (
             <div className='login'>
                 <header className='login-header'>
                     header
                 </header>
                 <section className='login-content'>
-                    logon
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Form.Item>
+                            {getFieldDecorator('username', {
+                                rules: [{ required: true, message: 'Please input your username!' }],
+                            })(
+                                <Input
+                                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    placeholder="Username"
+                                />,
+                            )}
+                        </Form.Item>
+                        <Form.Item>
+                            {getFieldDecorator('password', {
+                                rules: [{ required: true, message: 'Please input your Password!' }],
+                            })(
+                                <Input
+                                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    type="password"
+                                    placeholder="Password"
+                                />,
+                            )}
+                        </Form.Item>
+                        <Form.Item>
+                            {getFieldDecorator('remember', {
+                                valuePropName: 'checked',
+                                initialValue: true,
+                            })(<Checkbox>Remember me</Checkbox>)}
+                            <a className="login-form-forgot" href="">
+                                Forgot password
+                            </a>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                Log in
+                            </Button>
+                            Or <a href="">register now!</a>
+                        </Form.Item>
+                    </Form>
                 </section>
             </div>
         )
     }
 }
+
+const  Login = Form.create({ name: 'normal_login' })(LoginTag);
+
+export default Login
