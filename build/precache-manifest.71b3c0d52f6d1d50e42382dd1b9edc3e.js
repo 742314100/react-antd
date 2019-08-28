091598e56f3b85c11cbc547d68e5fb1996412ae9
742314100@ <742314100@qq.com>
2019-08-28 17:28:36 +0800

    2.16.1

---
+++ build/precache-manifest.71b3c0d52f6d1d50e42382dd1b9edc3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb4fc3154b0ffc942aeec986175be2af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc3a8d55ad8f06e97d53",
+    "url": "/static/css/2.3790a8c7.chunk.css"
+  },
+  {
+    "revision": "04e9ec7c65fd839c3fc6",
+    "url": "/static/css/main.97dd35b3.chunk.css"
+  },
+  {
+    "revision": "cc3a8d55ad8f06e97d53",
+    "url": "/static/js/2.ad1ae639.chunk.js"
+  },
+  {
+    "revision": "04e9ec7c65fd839c3fc6",
+    "url": "/static/js/main.2e72386d.chunk.js"
+  },
+  {
+    "revision": "cfebd996861efdaafe21",
+    "url": "/static/js/runtime~main.bc25adeb.js"
+  },
+  {
+    "revision": "429bff8933fd861a28452d7c78a3c565",
+    "url": "/static/media/bg.429bff89.jpg"
+  }
+]);
